c429578c70c34cf74a98303f49c81dcd576fdce1
Michal Vašut <michal.vasut@firma.seznam.cz>
2024-10-23 00:34:20 +0200

    Updates python example to use modern python 3

--- examples/authentication/python.py
+++ examples/authentication/python.py
@@ -1,145 +1,128 @@
-#!/usr/bin/python
-#
-# This is a bare-bones example of authenticating as a "known" (not interactive through a browser)
-# WikiTree member for use with the WikiTree API. In order to carry the authentication through,
-# a Session is maintained with https://api.wikitree.com which holds the session state via 
-# cookie, as a browser would.
-#
-#####################################################################
+"""
+This is a bare-bones example of authenticating as a "known" (not interactive through a browser) WikiTree member 
+for use with the WikiTree API. In order to carry the authentication through a Session is maintained 
+with https://api.wikitree.com which holds the session state via cookie, as a browser would.
+"""
 
-import requests
+from requests import Session
 import re
-import sys
 from getpass import getpass
+import os
+
+import urllib
+import json
 
 
-# Define the WikiTree API endpoint
+# WikiTree API endpoint
 API_URL = "https://api.wikitree.com/api.php"
 
-# Optionally define the WikiTree member to use for API requests.
-# These values should never be committed to a repository, and could be instead loaded
-# from a settings file, etc. If these are left blank, then they are queried for on the
-# command line.
-LOGIN_EMAIL = ''
-LOGIN_PASSWORD = ''
 
-#####################################################################
+def get_credentials() -> tuple[str, str]:
+    """Loads credentials from environment variables (if provided), otherwise asks user for them."""
 
-##########
-# If we don't have the login information, query the user for it.
-##########
-if (LOGIN_EMAIL):
-    email = LOGIN_EMAIL
-else:
-    email = raw_input("Email:")
+    email = os.environ.get("LOGIN_EMAIL") or input("Email: ")
+    password = os.environ.get("LOGIN_PASSWORD") or getpass(f"Password: ")
 
-if (LOGIN_PASSWORD):
-    password = LOGIN_PASSWORD
-else:
-    password = getpass("Password for "+email+":")
+    return email, password
 
 
-##########
-# We use a Session to hold the state (via cookie jar) for the API queries
-##########
-print "Starting Session.\n"
-apiSession = requests.Session()
+def authenticate_session(session: Session, email: str, password: str) -> None:
+    """Authenticates the session with provided credentials"""
 
-##########
-# Step 1 - POST the clientLogin action with our member credentials.
-#
-# We post with allow_redirect = False because we don't want the
-# Python request to automatically follow redirects. We want the initial response (still on api.wikitree.com)
-# and not the redirected-to destination.
-#
-# Note that the apiResponse here will, on successful login, be a redirection.
-# In a browser interaction we'd include a returnURL value in postData to send the browser
-# back to our app. Here we're just capturing the authcode from the Location redirection
-# directly.
-##########
-print "POSTing clientLogin(email,password)"
-postData = { 'action':'clientLogin', 'doLogin': 1, 'wpEmail': email, 'wpPassword': password }
-apiResponse = apiSession.post(API_URL, data=postData, allow_redirects=False, auth=('wikitree','wikitree'))
+    session.auth = ("wikitree", "wikitree")
 
+    # Step 1: Obtain the authcode - will be sent from the API in Location header
 
-##########
-# If we have a "Location" redirection with an authcode value as our response, then
-# the login was successful. Otherwise, we failed.
-##########
-if ((apiResponse.status_code != 302) or (apiResponse.headers['Location'] is None)):
-    print "clientLogin POST did not return expected 302 Redirect."
+    data = {
+        "action": "clientLogin",
+        "doLogin": 1,
+        "wpEmail": email,
+        "wpPassword": password,
+    }
+    resp = session.post(
+        API_URL,
+        data=data,
+        allow_redirects=False,  # necessary to POST without redirect, we need to capture the Location header
+    )
+    resp.raise_for_status()
 
-    # On failure, the response content will be a redisplay of the web page with the 
-    # API Client Login form.
-    #print "Response status_code = "+str(apiResponse.status_code)
-    #print ("Header: "+str(apiResponse.headers))
-    #print ("Content:"+apiResponse.text)
+    if (
+        resp.status_code != 302
+        or (location := resp.headers.get("Location")) is None
+        or "authcode" not in location
+    ):
+        print("Cannot authenticate with Wikitree API: authcode was not obtained")
+        exit()
 
-    print "\nDie\n";
-    sys.exit()
+    matches = re.search(r"authcode=(?P<authcode>.+$)", location).groupdict()
+    authcode = matches.get("authcode")
 
-matches = re.search('authcode=(.*)', apiResponse.headers['Location'])
-if (matches == None):
-    print "clientLogin POST failed - couldn't find authcode"
-    print "Response status_code = "+str(apiResponse.status_code)
-    print ("Header: "+str(apiResponse.headers))
-    print "\nDie\n"
-    sys.exit()
+    # Step 2: Send back the authcode to finish the authentication
 
-authcode = matches.group(1)
-print "clientLogin succeeded: authcode="+authcode
+    data = {"action": "clientLogin", "authcode": authcode}
+    resp = session.post(API_URL, data=data, allow_redirects=False)
+    resp.raise_for_status()
 
+    if not resp.ok:
+        print("Cannot authenticate with Wikitree API: failed the authcode verification")
+        exit()
 
-##########
-# Step 2 - POST back the authcode we got. This completes the login/session setup
-# at api.wikitree.com. Since we use the same Session for the post, the cookies are all 
-# saved. A success here is a 200 and we'll have WikiTree session cookies.
-##########
-print "POSTing clientLogin(authcode)"
-postData = { 'action': 'clientLogin', 'authcode': authcode }
-apiResponse = apiSession.post(API_URL, data=postData, allow_redirects=False)
-if (apiResponse.status_code != 200):
-    print "clientLogin(authcode) failed."
-    print "Response status_code = "+str(apiResponse.status_code)
-    print ("Header: "+str(apiResponse.headers))
-    print "\nDie\n"
-    sys.exit()
+    # Since we are communicating with the same session, cookies should be already set, example:
+    # {
+    #   'wikitree_wtb__session': '<<session_id>>',
+    #   'wikitree_wtb_Token': '<<token>>',
+    #   'wikidb_wtb_UserName': '<<WikiTree ID>>',
+    #   'wikitree_wtb_UserID': '<<WikiTree user_id>>'
+    # }
 
-# The cookies set by the API look like:
-# {'wikitree_wtb__session': '<<session_id>>', 'wikitree_wtb_Token': '<<token>>', 'wikidb_wtb_UserName': '<<WikiTree ID>>', 'wikitree_wtb_UserID': '<<WikiTree user_id>>'}
-wtCookie = apiSession.cookies.get_dict()
-if (wtCookie is None):
-    print "clientLogin(authcode) failed -- No Cookies."
-    print "\nDie\n"
-    sys.exit()
-print wtCookie
+    cookies = session.cookies.get_dict()
 
-#if ('wikidb_wtb_UserName' not in wtCookie):
-#   print "clientLogin(authcode) failed -- No WikiTree Session Info found in Cookies."
-#    print "\nDie\n"
-#    sys.exit()
+    if "wikidb_wtb__session" not in cookies:
+        print("Cannot authenticate with Wikitree API: cookies were not set properly")
+        exit()
 
-print "clientLogin(authcode) succeeded: "+wtCookie['wikidb_wtb_UserName']
+    print(
+        'User "'
+        + urllib.parse.unquote(cookies["wikidb_wtb_UserName"])
+        + '" is successfully authenticated!'
+    )
 
 
-##########
-# At this point the member should be logged into the API site, with their 
-# session data stored in a CookieJar in our Session. We can now query the 
-# API for data that is restricted to profiles with that member on the Trusted List.
-# As an example, get the logged-in member's profile data itself.
-##########
-key = wtCookie['wikidb_wtb_UserName']
-fields = 'Id,Name,FirstName,LastNameAtBirth,LastNameCurrent,BirthDate'
-print "\ngetProfile("+key+")"
-apiResponse = apiSession.post(API_URL, {'action':'getProfile', 'key':key, 'fields':fields}, auth=('wikitree','wikitree'))
+def prepare_session(email: str | None, password: str | None) -> Session:
+    """
+    Prepares the session for the communication with Wikitree API,
+    for authenticated access to API, fill in optional fields: email & password
+    """
 
-data = apiResponse.json()
-print "Data from JSON:"
-print data
-print "\n"
+    session: Session = Session()
 
-print "First Name: "+ data[0]['profile']['FirstName']
-print "Last Name: "+ data[0]['profile']['LastNameAtBirth']
-print "Birth Date: "+ data[0]['profile']['BirthDate']
+    if email and password:
+        authenticate_session(session, email, password)
 
-print "\n\nDone\n"
+    return session
+
+
+def load_profile(session: Session, key: str) -> dict[str, any]:
+    """Loads the info for the user specified by the key parameter"""
+
+    fields = "Id,Name,FirstName,LastNameAtBirth,LastNameCurrent,BirthDate"
+
+    print(f"\nPOST /api.php?getProfile={key}fields={fields}\n")
+    resp = session.post(API_URL, {"action": "getProfile", "key": key, "fields": fields})
+    resp.raise_for_status()
+
+    return resp.json()
+
+
+if __name__ == "__main__":
+    # email, password = None, None # uncomment to test without authentication
+    email, password = get_credentials()  # comment to test without authentication
+
+    print(f"Starting the session as" + (f'"{email}"' if email else "unauthenticated user") + ".")
+    session = prepare_session(email, password)
+
+    # As an example, get the logged-in member's profile data itself
+    key = session.cookies.get("wikidb_wtb_UserName") or "Windsor-1" # uses Windsor-1 when not authenticated
+    profile = load_profile(session, urllib.parse.unquote(key))
+
+    print(json.dumps(profile, indent=2, ensure_ascii=False))
